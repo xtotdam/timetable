2629c7c5a3d828d036024423f93db8df9134645d
xtotdam <xtotdam@yandex.ru>
2017-02-07 22:23:37 +0300

    fix old windows encoding issue add cell with long subject name

--- data.py
+++ data.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-# import sys
-# reload(sys)  # Reload does the trick!
-# sys.setdefaultencoding('UTF8')
 
 '''Memo:
 \\ppp{What}{Where}{Who}
+\\pppl{What}{Where}{Who}            % long name
+
 \\ppps{What}
+
 \\pppf{What}{Where}{Who}{Неч/Ч}
+\\pppfl{What}{Where}{Who}{Неч/Ч}    % long name
+
 \\ppppp{What1}{Where1}{Who1}{What2}{Where2}{Who2}
+
 \\blue \\green \\orange \\magenta \\yellow \\violet
 '''
 
@@ -42,24 +45,24 @@ numbers = [
 "\ddd{5}{17:05}{18:40}"
 ]
 
-out = open('data.tex', 'w')
-out.write("%% This file is generated automatically. Do not edit it.\n\n")
+with open('data.tex', 'w', encoding='utf8') as out:
+    out.write("%% This file is generated automatically. Do not edit it.\n\n")
 
-for i in range(5):
-	a = monday[i] if monday[i] else "\\ppps{~}"
-	b = tuesday[i] if tuesday[i] else "\\ppps{~}"
-	c = wednesday[i] if wednesday[i] else "\\ppps{~}"
-	d = thursday[i] if thursday[i] else "\\ppps{~}"
-	e = friday[i] if friday[i] else "\\ppps{~}"
-	f = saturday[i] if saturday[i] else "\\ppps{~}"
-	if i == 1:
-		end = ' \n\n\\\\ \\hline\\hline\n\n'
-	elif i == 4:
-		end = ''
-	else:
-		end = ' \n\n\\\\ \\hline\n\n'
+    for i in range(5):
+        a = monday[i] if monday[i] else "\\ppps{~}"
+        b = tuesday[i] if tuesday[i] else "\\ppps{~}"
+        c = wednesday[i] if wednesday[i] else "\\ppps{~}"
+        d = thursday[i] if thursday[i] else "\\ppps{~}"
+        e = friday[i] if friday[i] else "\\ppps{~}"
+        f = saturday[i] if saturday[i] else "\\ppps{~}"
+        if i == 1:
+            end = ' \n\n\\\\ \\hline\\hline\n\n'
+        elif i == 4:
+            end = ''
+        else:
+            end = ' \n\n\\\\ \\hline\n\n'
 
-	out.write(numbers[i] + ' &\n' +
-			a + ' &\n' + b + ' &\n' + c + ' &\n' + d + ' &\n' + e + ' &\n' + f + end)
+        out.write(numbers[i] + ' &\n' +
+                a + ' &\n' + b + ' &\n' + c + ' &\n' + d + ' &\n' + e + ' &\n' + f + end)
 
 out.close()
